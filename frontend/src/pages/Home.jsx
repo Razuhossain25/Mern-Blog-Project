731919dc35328c9600fa57217ce7e311f3b720c7
ashik1900-githib <ashif1900@gmail.com>
2026-01-27 16:52:31 +0600

    added a paragraph

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
             <Hero />
             <div className="container mx-auto px-5 md:px-0 py-3 md:py-6">
                 <h2 className="text-2xl md:text-4xl font-bold mb-4 text-center">Latest Articles</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In vitae ex exercitationem facere tempora impedit esse ea minima sint sapiente.</p>
             </div>
         </div>
     );
